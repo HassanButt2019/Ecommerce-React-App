2d5385f79d0b45ad21848aa97f926d9c8976e24d
Hassan Rauf Butt <hassanraufbutt@Hassans-MacBook-Air.local>
2022-02-16 02:47:40 +0500

    implemented video 8

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import { Routes , Route } from "react-router-dom";
 import Login from "./pages/authentication/Login";
 import Signup from "./pages/authentication/Signup";
 import Home from "./pages/home/Home";
+import Header from "./components/nav/header"
 
 
 const App = ()=> {
   return (
+
+    <>
+    <Header/>
     <Routes>
       <Route exact path="/" element={<Home/>}/>
       <Route exact path="/login" element={<Login/>}/>
       <Route exact path="/register" element={<Signup/>}/>
 
     </Routes>
+     </>
+
+
 
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Random from "./random";
 import { BrowserRouter} from "react-router-dom";
-
+import "antd/dist/antd.css"
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
     <App />
-    <Random/>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
